50316a2353d1a8ec2e071d51049235365aa549ab
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2023-10-17 23:57:58 +0200

    feat: Adjust home container

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,15 +1,16 @@
 import PageTitle from "@/components/PageTitle";
 import "@/app.css";
 import { ButtonLink } from "@/components/ButtonLink";
+import { PageContainer } from "@/components/PageContainer";
 
 export default function Home() {
   return (
-    <main className="container mx-auto">
+    <PageContainer>
       <PageTitle>Kids & Workshops</PageTitle>
       <div className="flex items-center justify-center gap-x-12">
         <ButtonLink to="/kids">Kids</ButtonLink>
         <ButtonLink to="/workshops">Workshops</ButtonLink>
       </div>
-    </main>
+    </PageContainer>
   );
 }
